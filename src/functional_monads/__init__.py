ab8c3869d2c949ce60e406f368d57db68c985c8a
Tobias Bollinger <tobias.bollinger@gmail.com>
2025-02-18 16:15:30 +0100

    refactor versioning system to use version-pioneer and remove __about__.py

--- src/functional_monads/__init__.py
+++ src/functional_monads/__init__.py
@@ -2,3 +2,8 @@ from .either import Either, left, right
 from .maybe import Maybe, nothing, just
 
 __all__ = ["Either", "left", "right", "Maybe", "nothing", "just"]
+
+# src/my_project/__init__.py
+from ._version import get_version_dict
+
+__version__ = get_version_dict()["version"]
